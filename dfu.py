106f6397c19284427a65983dc840c9ceee4de78b
nanovna <54525305+nanovna@users.noreply.github.com>
2020-01-11 10:22:47 +0000

    dfu.py: fix hardcoded paths

--- dfu.py
+++ dfu.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import serial, tty, sys
+import argparse
 
 # returns true if device is in dfu mode
 def detectDFU(ser):
@@ -52,7 +53,30 @@ def waitSend(ser):
 	sys.stdout.write('.')
 	sys.stdout.flush()
 
-ser = serial.Serial('/dev/ttyACM0')
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--serial', default='/dev/ttyACM0', 
+	help="Path to USB serial device")
+parser.add_argument('-f', '--file',
+	help="Path to .bin image file")
+parser.add_argument('-r', '--reboot', action='store_true', 
+	help="Reset device without flashing any image")
+parser.add_argument('-a', '--argument', type=lambda x:int(x,0), default=-1, 
+	help="User argument (u32) passed to application after reset")
+args = parser.parse_args()
+
+if args.file != None and args.reboot:
+	print('-f and -r can not both be specified at the same time')
+	exit(1)
+
+if args.file == None and not args.reboot:
+	print('Either -f or -r required')
+	exit(1)
+
+
+ser = serial.Serial(args.serial)
 tty.setraw(ser.fd)
 ser.timeout = 0.1
 ser.read(4096)
@@ -63,27 +87,32 @@ if not detectDFU(ser):
 	print('Please enter DFU mode through menu CONFIG -> DFU or hold down the JOG LEFT button and power cycle the device.')
 	exit(1)
 
-sendWriteAddr(ser, 0x08004000)
+if args.file != None:
+	sendWriteAddr(ser, 0x08004000)
 
-src = sys.stdin
-outstanding = 0
-while True:
-	buf = src.buffer.read(1200)
-	if len(buf) == 0:
-		break
-	sendBytes(ser, buf)
-	outstanding += 1
-	if outstanding >= 2:
+	src = open(args.file)
+	outstanding = 0
+	while True:
+		buf = src.buffer.read(1200)
+		if len(buf) == 0:
+			break
+		sendBytes(ser, buf)
+		outstanding += 1
+		if outstanding >= 5:
+			waitSend(ser)
+			outstanding -= 1
+
+	while outstanding > 0:
 		waitSend(ser)
 		outstanding -= 1
 
-while outstanding > 0:
-	waitSend(ser)
-	outstanding -= 1
+	print('')
+	print('done. Resetting')
 
-
-print('')
-print('done. Resetting')
+# set user argument
+if args.argument >= 0:
+	cmd = b"\x22\xe8" + int.to_bytes(args.argument, 4, 'little')
+	ser.write(cmd)
 
 # reboot device
 ser.write([0x20, 0xef, 0x5e])
