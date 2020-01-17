69a5f0f31543a3f8f139ac5ce5d5461be3208015
nanovna <54525305+nanovna@users.noreply.github.com>
2020-01-17 04:23:35 +0000

    add board_v2_2 as an alias to board_v2_1

---
+++ board_v2_2/board.hpp
@@ -0,0 +1,9 @@
+
+// v2_2 board is functionally identical to v2_1, so reuse the code
+#include "../board_v2_1/board.hpp"
+
+// change board name
+#undef BOARD_NAME
+#undef BOARD_REVISION
+#define BOARD_NAME "NanoVNA V2_2"
+#define BOARD_REVISION (2)
