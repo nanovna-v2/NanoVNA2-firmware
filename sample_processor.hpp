ed534e8af2b7a5c688c5b254990aab14ceaddb86
DiSlord <dislord@mail.ru>
2020-11-22 14:02:34 +0300

    Add DSP instruction for sampling Accumulate result in complex float

--- sample_processor.hpp
+++ sample_processor.hpp
@@ -1,4 +1,24 @@
+// Cortex M4 DSP instructions assembly
 
+// __smlabb inserts a SMLABB instruction. __smlabb returns the equivalent of
+//  int32_t res = x[0] * y[0] + acc
+//  where [0] is the lower 16 bits and [1] is the upper 16 bits. This operation sets the Q flag if overflow occurs on the addition.
+__attribute__((always_inline)) static inline int32_t __smlabb(int32_t x, int32_t y, int32_t acc)
+{
+  int32_t r;
+  __asm volatile ("smlabb %[r], %[x], %[y], %[a]" : [r] "=r" (r) : [x] "r" (x), [y] "r" (y), [a] "r" (acc) : );
+  return r;
+}
+
+// __smlabt inserts a SMLABT instruction. __smlabt returns the equivalent of
+//  int32_t res = x[0] * y[1] + acc
+//  where [0] is the lower 16 bits and [1] is the upper 16 bits. This operation sets the Q flag if overflow occurs on the addition.
+__attribute__((always_inline)) static inline int32_t __smlabt(int32_t x, int32_t y, int32_t acc)
+{
+  int32_t r;
+  __asm volatile ("smlabt %[r], %[x], %[y], %[a]" : [r] "=r" (r) : [x] "r" (x), [y] "r" (y), [a] "r" (acc) : );
+  return r;
+}
 
 // nStreams specifies the number of interleaved streams in the incoming data.
 // emitValue_t must have the signature:
@@ -37,22 +57,28 @@ public:
 	// the number of words in the array must be len * nStreams).
 	// returns whether we completed a cycle.
 	bool process(uint16_t* samples, int len) {
-		uint16_t* end = samples+len*nStreams;
 		bool ret = false;
-		while(samples < end) {
+		len*=nStreams;
+		while(len--) {
+#if 1
+			int32_t sc_t = ((int32_t *)correlationTable)[accumPhase];
+			for(int streamNum = 0; streamNum < nStreams; streamNum++){
+				int32_t sample = *samples++ - 2048;                              // 12bit ADC
+				if(sample > 2000 || sample < -2000) {clipFlag = true;}           // Overflow
+				accumRe[streamNum] = __smlabt(sample, sc_t, accumRe[streamNum]); // accumRe[streamNum]+= sample[0] * sc_t[1];
+				accumIm[streamNum] = __smlabb(sample, sc_t, accumIm[streamNum]); // accumIm[streamNum]+= sample[0] * sc_t[0];
+			}
+#else
 			int32_t lo_im = correlationTable[accumPhase*2];
 			int32_t lo_re = correlationTable[accumPhase*2 + 1];
 			for(int streamNum = 0; streamNum < nStreams; streamNum++) {
-				int16_t sample = int16_t((samples[streamNum])*16 - 32768);
-				if(sample > 30000) clipFlag = true;
-				if(sample < -30000) clipFlag = true;
-			
-				accumRe[streamNum] += lo_re*sample/8192;
-				accumIm[streamNum] += lo_im*sample/8192;
+				int32_t sample = *samples++ - 2048;                       // 12bit ADC
+				if(sample > 2000 || sample < -2000) {clipFlag = true;}    // Overflow
+				accumRe[streamNum] += lo_re*sample;
+				accumIm[streamNum] += lo_im*sample;
 			}
-			
+#endif
 			accumPhase++;
-			samples += nStreams;
 			if(int(accumPhase) >= accumPeriod) {
 				emitValue(accumRe, accumIm);
 				for(int streamNum = 0; streamNum < nStreams; streamNum++)
